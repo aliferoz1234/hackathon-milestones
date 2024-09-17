486ce28d22c0898f93ee2fc3da33cb4d05045cf0
aliferoz1234 <159272761+aliferoz1234@users.noreply.github.com>
2024-09-17 15:47:15 +0500

    Add files via upload

--- Dynamic Resume Milestone-03/script.js
+++ Dynamic Resume Milestone-03/script.js
@@ -1,7 +1,10 @@
 var _a;
 (_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    var _a;
     event.preventDefault(); //prevent page reload
     //collect input values
+    var profilePictureInput = (_a = document.getElementById("profilePicture").files) === null || _a === void 0 ? void 0 : _a[0];
+    var profilePictureURL = profilePictureInput ? URL.createObjectURL(profilePictureInput) : "";
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var phone = document.getElementById('phone').value;
@@ -9,7 +12,7 @@ var _a;
     var experience = document.getElementById('experience').value;
     var skills = document.getElementById('skills').value;
     // resume output
-    var resumeOutput = "<h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Phone:</b>").concat(email, "</p>\n    <p><b>phone:</b>").concat(phone, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>");
+    var resumeOutput = "\n    <h2><b>Resume</b></h2>\n    <img src=\"".concat(profilePictureURL, "\" alt=\"Profle Picture\" class=\"profilePicture\">\n    <h3>Personal Information</h3>\n    <p><b>Name: </b>").concat(name, "</p>\n    <p><b>Email: </b>").concat(email, "</p>\n    <p><b>Phone: </b>").concat(phone, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>");
     var resumeOutputElement = document.getElementById('resumeDisplay');
     if (resumeOutputElement) {
         resumeOutputElement.innerHTML = resumeOutput;
--- Dynamic Resume Milestone-03/script.ts
+++ Dynamic Resume Milestone-03/script.ts
@@ -3,21 +3,24 @@ document.getElementById('resumeForm')?.addEventListener('submit',function(event)
 
  
 //collect input values
+    const profilePictureInput=(document.getElementById("profilePicture") as HTMLInputElement).files?.[0]
+    const profilePictureURL=profilePictureInput?URL.createObjectURL(profilePictureInput):"";
     const name=(document.getElementById('name') as HTMLInputElement).value;
     const email=(document.getElementById('email') as HTMLInputElement).value;
     const phone=(document.getElementById('phone') as HTMLInputElement).value;
-    const education=(document.getElementById('education') as HTMLInputElement).value;
-    const experience=(document.getElementById('experience') as HTMLInputElement).value;
-    const skills=(document.getElementById('skills') as HTMLInputElement).value;
+    const education=(document.getElementById('education') as HTMLTextAreaElement).value;
+    const experience=(document.getElementById('experience') as HTMLTextAreaElement).value;
+    const skills=(document.getElementById('skills') as HTMLTextAreaElement).value;
 
    
     // resume output
-    const resumeOutput=
-    `<h2><b>Resume</b></h2>
+    const resumeOutput=`
+    <h2><b>Resume</b></h2>
+    <img src="${profilePictureURL}" alt="Profle Picture" class="profilePicture">
     <h3>Personal Information</h3>
-    <p><b>Name:</b>${name}</p>
-    <p><b>Phone:</b>${email}</p>
-    <p><b>phone:</b>${phone}</p>
+    <p><b>Name: </b>${name}</p>
+    <p><b>Email: </b>${email}</p>
+    <p><b>Phone: </b>${phone}</p>
 
     <h3>Education</h3>
     <p>${education}</p>
